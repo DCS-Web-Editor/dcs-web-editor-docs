22f1b76dcaccee2001eb65c519001a8c6705042b
GhostCarousal <110270748+GhostCarousal@users.noreply.github.com>
2024-04-26 11:11:15 -0600

    update 3d script installer

--- docs/editor/scripts/3D-Script-Installer.ps1
+++ docs/editor/scripts/3D-Script-Installer.ps1
@@ -1,19 +1,15 @@
+### DCS Web Editor 3D Script Installer by CourtesyFlush
+### Contributers: Guillaume
+
 # Initial Functions
 
 ## Create log file
-$LogFile = ".\DCSWE3D.log"
-
-function Write-Log {
-  Param([string]$LogString)
-  $Stamp = (Get-Date).ToString("yyyy/MM/dd HH:mm:ss")
-  $LogMessage = "$Stamp $LogString"
-  Add-Content $LogFile -Value $LogMessage
-}
+$LogFile = ".\3D-Script-Installer.log"
 
 Start-Transcript -Append $LogFile
 
 if (Test-Path $LogFile) {
-  Write-Host "Log file $LogFile created successfully." -f Green
+  Write-Host "Log file $LogFile created successfully." -ForegroundColor Green
 }
 else {
   Read-Host -Prompt "Log file not created."
@@ -29,12 +25,12 @@ function Exit-Script {
 # Set pathing variables
 
 ## Test MissionScripting path
-Write-Host "Testing MissionScripting.lua path..." -f Yellow
+Write-Host "Testing MissionScripting.lua path..." -ForegroundColor Yellow
 
 $MissionScriptPath = ".\Scripts\MissionScripting.lua"
 
 if (Test-Path "$MissionScriptPath") {
-  Write-host "Found the MissionScripting.lua." -f Green
+  Write-host "Found the MissionScripting.lua." -ForegroundColor Green
 }
 else {
   Exit-Script
@@ -42,25 +38,35 @@ else {
 
 ## Get user name to find Saved Games folder
 $MyName = [Environment]::UserName
-Write-Host "User $MyName found, adding to Saved Games path." -f Green
+Write-Host "User $MyName found, adding to Saved Games path." -ForegroundColor Green
 
-## Set Saved Games path variable
-Write-Host "Testing Saved Games path..." -f Yellow
+## Initialize variables for beta and release paths
+$betaPath = "C:\Users\$MyName\Saved Games\DCS.openbeta"
+$releasePath = "C:\Users\$MyName\Saved Games\DCS"
 
-if (Test-Path "C:\Users\$MyName\Saved Games\DCS.openbeta") {
-  $SavedGamesPath = "C:\Users\$MyName\Saved Games\DCS.openbeta"
-  Write-Host "Found Saved Games folder at $SavedGamesPath." -f Green
-}
-elseif (Test-Path "C:\Users\$MyName\Saved Games\DCS") {
-  $SavedGamesPath = "C:\Users\$MyName\Saved Games\DCS"
-  Write-Host "Found Saved Games folder at $SavedGamesPath." -f Green
-}
-else {
+## Check for the presence of beta and release directories
+$hasBeta = Test-Path $betaPath
+$hasRelease = Test-Path $releasePath
+
+## Set the default saved games path and output the appropriate message
+if ($hasBeta -or $hasRelease) {
+  if ($hasBeta -and $hasRelease) {
+    Write-Host "Both DCS.openbeta and DCS folders found, installing in DCS folder." -ForegroundColor Yellow
+    $SavedGamesPath = $releasePath
+  } elseif ($hasBeta) {
+    Write-Host "Found Saved Games folder at $betaPath." -ForegroundColor Green
+    $SavedGamesPath = $betaPath
+  } else {
+    Write-Host "Found Saved Games folder at $releasePath." -ForegroundColor Green
+    $SavedGamesPath = $releasePath
+  }
+} else {
+  Write-Host "No DCS Saved Games folder found, exiting script." -ForegroundColor Red
   Exit-Script
 }
 
 ## Check for Scripts folder in DCS.openbeta Saved Games
-Write-Host "Checking for Scripts folder in DCS Saved Games..." -f Yellow
+Write-Host "Checking for Scripts folder in DCS Saved Games..." -ForegroundColor Yellow
 
 ### Function for testing the Scripts folder path
 function Find-Scripts {
@@ -69,11 +75,11 @@ function Find-Scripts {
 
 ### Function for creating the Scripts folder and retesting the path
 function New-Scripts {
-  Write-Host "Creating Scripts folder..." -f Yellow
+  Write-Host "Creating Scripts folder..." -ForegroundColor Yellow
   New-Item -Path "$SavedGamesPath\Scripts" -ItemType Directory
   New-Item -Path "$SavedGamesPath\Scripts\Hooks" -ItemType Directory
   if (Find-Scripts) {
-    Write-Host "Scripts folder successfully created." -f Green
+    Write-Host "Scripts folder successfully created." -ForegroundColor Green
   }
   else {
     Exit-Script
@@ -82,15 +88,15 @@ function New-Scripts {
 
 ### Test for the Scripts folder
 if (Find-Scripts) {
-  Write-Host "Scripts folder found." -f Green
+  Write-Host "Scripts folder found." -ForegroundColor Green
 }
 else {
-  Write-Host "Scripts folder not found." -f Red
+  Write-Host "Scripts folder not found." -ForegroundColor Red
   New-Scripts
 }
 
 ## Check for the Hooks folder in DCS.openbeta Saved Games
-Write-Host "Checking for Hooks folder in DCS Saved Games..." -f Yellow
+Write-Host "Checking for Hooks folder in DCS Saved Games..." -ForegroundColor Yellow
 
 ### Function for testing the Hooks folder path
 function Find-Hooks {
@@ -99,10 +105,10 @@ function Find-Hooks {
 
 ### Function for creating the Hooks folder and retesting the path
 function New-Hooks {
-  Write-Host "Creating Hooks folder..." -f Yellow
+  Write-Host "Creating Hooks folder..." -ForegroundColor Yellow
   New-Item -Path "$SavedGamesPath\Scripts\Hooks" -ItemType Directory
   if (Find-Hooks) {
-    Write-Host "Hooks folder successfully created." -f Green
+    Write-Host "Hooks folder successfully created." -ForegroundColor Green
   }
   else {
     Exit-Script
@@ -110,10 +116,10 @@ function New-Hooks {
 }
 
 if (Find-Hooks) {
-  Write-Host "Hooks folder found." -f Green
+  Write-Host "Hooks folder found." -ForegroundColor Green
 }
 else {
-  Write-Host "Hooks folder not found." -f Red
+  Write-Host "Hooks folder not found." -ForegroundColor Red
   New-Hooks
 }
 
@@ -121,27 +127,27 @@ else {
 
 ## Download functions
 function Get-MissionScripting {
-  Write-Host "Downloading MissionScripting.lua..." -f Yellow
+  Write-Host "Downloading MissionScripting.lua..." -ForegroundColor Yellow
   Invoke-WebRequest "https://dcs-web-editor.github.io/dcs-web-editor-docs/editor/scripts/MissionScripting.lua" -OutFile "$MissionScriptPath"
 }
 
 function Get-MissionServer {
-  Write-Host "Downloading dcs-web-editor-mission-server.lua..." -f Yellow
+  Write-Host "Downloading dcs-web-editor-mission-server.lua..." -ForegroundColor Yellow
   Invoke-WebRequest "https://dcs-web-editor.github.io/dcs-web-editor-docs/editor/scripts/dcs-web-editor-mission-server.lua" -OutFile "$SavedGamesPath\Scripts\dcs-web-editor-mission-server.lua"
 }
 
 function Get-GUIServer {
-  Write-Host "Downloading dcs-web-editor-gui-server.lua..." -f Yellow
+  Write-Host "Downloading dcs-web-editor-gui-server.lua..." -ForegroundColor Yellow
   Invoke-WebRequest "https://dcs-web-editor.github.io/dcs-web-editor-docs/editor/scripts/dcs-web-editor-gui-server.lua" -OutFile "$SavedGamesPath\Scripts\Hooks\dcs-web-editor-gui-server.lua"
 }
 
 ## Functions - verify scripts after download
 function Test-MissionScripting {
-  Write-Host "Verifying MissionScripting.lua download..." -f Yellow
+  Write-Host "Verifying MissionScripting.lua download..." -ForegroundColor Yellow
   $ScriptToken = Get-Content "$MissionScriptPath" | Select-String "package.path"
   $MissionScriptVariable = 'package.path = package.path .. ";.\\LuaSocket\\?.lua"'
   if ($ScriptToken -like $MissionScriptVariable) {
-    Write-Host "Verified." -f Green
+    Write-Host "Verified." -ForegroundColor Green
   }
   else {
     Exit-Script
@@ -149,9 +155,9 @@ function Test-MissionScripting {
 }
 
 function Test-MissionServer {
-  Write-Host "Verifying dcs-web-editor-mission-server.lua download..." -f Yellow
+  Write-Host "Verifying dcs-web-editor-mission-server.lua download..." -ForegroundColor Yellow
   if (Test-Path "$SavedGamesPath\Scripts\dcs-web-editor-mission-server.lua") {
-    Write-Host "Verified." -f Green
+    Write-Host "Verified." -ForegroundColor Green
   }
   else {
     Exit-Script
@@ -159,9 +165,9 @@ function Test-MissionServer {
 }
 
 function Test-GUIServer {
-  Write-Host "Verifying dcs-web-editor-gui-server.lua download..." -f Yellow
+  Write-Host "Verifying dcs-web-editor-gui-server.lua download..." -ForegroundColor Yellow
   if (Test-Path "$SavedGamesPath\Scripts\Hooks\dcs-web-editor-gui-server.lua") {
-    Write-Host "Verified." -f Green
+    Write-Host "Verified." -ForegroundColor Green
   }
   else {
     Exit-Script
@@ -169,7 +175,7 @@ function Test-GUIServer {
 }
 
 # Download the files
-Write-Host "Starting file downloads..." -f Yellow
+Write-Host "Starting file downloads..." -ForegroundColor Yellow
 
 Get-MissionServer
 Get-GUIServer
@@ -180,6 +186,6 @@ Test-GUIServer
 Test-MissionScripting
 
 #Installation Complete
-Write-host "Installation Complete." -f Green
+Write-host "Installation Complete." -ForegroundColor Green
 Read-Host -Prompt "Press Enter to exit Powershell."
 exit
